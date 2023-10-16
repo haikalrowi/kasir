14f20cc92ed4939245f39b54ecc5e3e423f94be8
ei <ei@teyvat>
2023-10-16 18:43:44 +0700

    Fix slug

--- src/app/api/slug/route.ts
+++ src/app/api/slug/route.ts
@@ -1,28 +1,19 @@
 import prisma from "@/lib/prisma";
 
-async function SLUG() {
-  const { product, receipt, receiptItem } = prisma;
-
-  const slug = await receipt.findUnique({
-    where: { id: "${id}" },
-    include: { ReceiptItem: { include: { Product: true } } },
-  });
-
-  const slug2 = await receipt.findFirst({
-    where: { finished: false },
-    include: { ReceiptItem: { include: { Product: true } } },
-  });
-}
-
 export async function POST(request: Request) {
-  const SLUG_KEY = process.env.SLUG_KEY;
   const { product, receipt, receiptItem } = prisma;
+  const SLUG_KEY = process.env.SLUG_KEY;
+  const models = ["product", "receipt", "receiptItem"];
+  const prismaQuery = await request.text();
 
-  if (request.headers.get("Slug-Key") === SLUG_KEY) {
+  if (
+    request.headers.get("Slug-Key") === SLUG_KEY &&
+    models.some((model) => prismaQuery.startsWith(`${model}.`))
+  ) {
     try {
-      return Response.json(await eval(await request.text()));
-    } catch {
-      return Response.json(null);
+      return Response.json(await eval(prismaQuery));
+    } catch (error) {
+      return Response.json({ error });
     }
   }
 
